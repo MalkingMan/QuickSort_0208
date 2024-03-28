dfe6ce0c54d0a581dcd836a2296b62a19fe5bd0b
MalkingMan <arrayalkhozini@gmail.com>
2024-03-28 13:50:38 +0700

    commit 2

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -7,17 +7,19 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
-void input()
-{
+void input(){
 while (true)
-
 {
-
 cout << "Masukan panjang element array: ";
 cin >> n;
 
 if (n <= 20)
-break;
+    break;
 else
-cout << "\nMaksimum panjang array adalah 20" << endl;
+    cout << "\nMaksimum panjang array adalah 20" << endl;
 }
+
+cout << "\n------------------- << endl;
+cout << "\nEnter Array Element" << endl;
+cout << "\n-------------------<< endl;
+
